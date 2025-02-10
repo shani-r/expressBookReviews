b96a6c3e90f31918c9636ab2c87ef21d8b3ae252
shani-r <ramoopshani@gmail.com>
2025-02-10 13:18:09 -0500

    update book by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let booksList=Object.values(books)
+  let book = booksList.find(b => b.author===author);
+ 
+    if (book) {
+     let bookDetails = JSON.stringify(book);
+     res.send(`Book details for author ${author}: ${bookDetails}`);
+   } else {
+     res.send(`No book found for author ${author}`);}
 });
 
 // Get all books based on title
